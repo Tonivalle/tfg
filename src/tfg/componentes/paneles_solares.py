46d0002c304d6ea5129b02559560e49997c269f7
Tonivalle <tonivalle15@gmail.com>
2024-12-08 19:53:15 +0100

    paneles

--- src/tfg/componentes/paneles_solares.py
+++ src/tfg/componentes/paneles_solares.py
@@ -5,16 +5,16 @@ class PanelSolar(BaseModel):
     tamaño: float
     eficiencia: float
 
-    def calcular_generacion(self, serie_irradiacion: list[float]) -> float:
+    def calcular_generacion_serie(self, serie_irradiacion: list[float]) -> float:
         """
         Calcula la energía generada total (W) mediante una serie de irradiacion por hora.
         """
         return sum(
-            self.calcular_generacion_estimada_por_hora(irradiacion)
+            self.calcular_generacion_estimada_hora(irradiacion)
             for irradiacion in serie_irradiacion
         )
 
-    def calcular_generacion_estimada_por_hora(self, irradiacion: float) -> float:
+    def calcular_generacion_estimada_hora(self, irradiacion: float) -> float:
         """
         Calcula la energía generada por hora.
 
--- src/tfg/frontend/display/vivienda.py
+++ src/tfg/frontend/display/vivienda.py
@@ -2,12 +2,16 @@ from tfg.componentes.vivienda import Vivienda
 from tfg.frontend.display.electrodomestico import DisplayElectrodomestico
 import streamlit as st
 
+from tfg.frontend.display.paneles_solares import DisplayPanel, MockIrradiacionAdapter
+
 class DisplayVivienda:
     def __init__(self, vivienda:Vivienda) -> None:
         self.vivienda = vivienda
         self.dis_electrodomesticos = [DisplayElectrodomestico(electrodomestico) for electrodomestico in self.vivienda.electrodomesticos]
-    
+        self.dis_paneles = [DisplayPanel(panel, MockIrradiacionAdapter()) for panel in self.vivienda.paneles_solares]
+
     def display(self):
+        self.display_paneles()
         self.display_electrodomesticos()
 
     def display_electrodomesticos(self):
@@ -16,6 +20,17 @@ class DisplayVivienda:
             dis_electrodomestico.formatear_fila_componente()
         st.divider()
         st.markdown(f'#### Consumo Total: {self._consumo_actual()} kWh')
-        
+
+    def display_paneles(self):
+        st.header(":blue[Paneles Solares]", divider="blue")
+        for idx, dis_paneles in enumerate(self.dis_paneles):
+            dis_paneles.formatear_fila_componente(idx)
+        st.divider()
+        st.markdown(f'#### Generación Total: {self._generacion_actual()} kWh')
+
     def _consumo_actual(self) -> float:
         return sum(dis_electrodomestico.consumo for dis_electrodomestico in self.dis_electrodomesticos)
+    
+    def _generacion_actual(self) -> float:
+        return sum(dis_panel.calcular_generacion() for dis_panel in self.dis_paneles)
+    
