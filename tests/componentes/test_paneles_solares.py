23a5ee73846da88726cf19f162dffda7f1009eb1
Tonivalle <tonivalle15@gmail.com>
2024-12-08 12:03:04 +0100

    paneles

---
+++ tests/componentes/test_paneles_solares.py
@@ -0,0 +1,16 @@
+from tfg.componentes.paneles_solares import PanelSolar
+
+
+class TestPanelesSolares:
+    def test_calculate_predicted_generation(self):
+        panel = PanelSolar(tamaño=5, eficiencia=0.75)
+
+        assert panel.calcular_generacion_estimada_por_hora(2381) == 8928.75
+
+    def test_calculate_generation(self):
+        panel = PanelSolar(tamaño=5, eficiencia=0.75)
+
+        assert (
+            panel.calcular_generacion([43, 188, 318, 394, 435, 410, 330, 205, 58])
+            == 8928.75
+        )
